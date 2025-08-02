a9667d37a6dbb3ba4193b3374982ea3fd9ed0e54
elvarsupply <hey@elvar.supply>
2025-08-02 18:06:38 +0000

    Test deploy

--- commands/linkdrop.js
+++ commands/linkdrop.js
@@ -6,7 +6,7 @@ module.exports = function (bot) {
   bot.command('linkdrop', (ctx) => {
     const id = ctx.from.id;
     state[id] = { step: 'awaiting_link' };
-    ctx.reply('🔗 Drop the Apollo URL to scrape:');
+    ctx.reply('Drop the Apollo URL to scrape 👇');
   });
 
   bot.on('text', async (ctx) => {
